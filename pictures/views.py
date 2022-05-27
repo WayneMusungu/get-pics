e3907569613ea4145112fc4d2478d21b08caadc4
WayneMusungu <waynemuyera17@gmail.com>
2022-05-27 21:32:45 +0300

    Create first template and pass in the date

--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 # Create your views here.
 
-def welcome(request):
-    return HttpResponse('Welcome to Pata Picha')
+def main(request):
+    date = dt.date.today()
+    return render(request, 'main.html',{"date": date})
