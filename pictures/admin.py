fae69091cca4d351ac02c4e2dd5a90cd758410f4
WayneMusungu <waynemuyera17@gmail.com>
2022-05-28 09:00:33 +0300

    Add a search form that will search by category

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 
-from pictures.models import Galleria, Location
+from pictures.models import Galleria, Location, Category
 
 
 # Register your models here.
 
 admin.site.register(Galleria)
 admin.site.register(Location)
+admin.site.register(Category)
