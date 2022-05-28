299c038769546bec0b290cf504ba898740500eae
WayneMusungu <waynemuyera17@gmail.com>
2022-05-28 07:43:00 +0300

    Create a location model and register it to the admin

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
+
 from pictures.models import Galleria
+from .models import Location
+
 
 admin.site.register(Galleria)
+admin.site.register(Location)
+
 
 # Register your models here.
--- pictures/models.py
+++ pictures/models.py
@@ -1,8 +1,34 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length =30)
+    def __str__(self):
+        return self.location
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
+
+
 class Galleria(models.Model):
     title = models.CharField(max_length=60)
     image_pic = models.ImageField(upload_to = 'pics/', null=True)
     description = models.TextField()
+    location = models.ManyToManyField('location')
     pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+    def save_galleria(self):
+        self.save()
+    def delete_galleria(self):
+        self.delete()
+        
+    @classmethod
+    def days_post(cls):
+        today = dt.date.today()
+        post = cls.objects.filter(pub_date__date = today)
+        return post
