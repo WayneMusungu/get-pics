1bc6baf7b35e9388a0ce2dbdb88bc05aee0f25ad
WayneMusungu <waynemuyera17@gmail.com>
2022-05-31 10:34:03 +0300

    Create a category test

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,20 @@
+from unicodedata import category
 from django.test import TestCase
+from .models import Category, Location, Galleria
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.culture= Category()
+        
+     # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.culture,Category))
+        
+        # Testing Save Method
+    def test_save_method(self):
+        self.culture.save_category()
+        categorys = Category.objects.all()
+        self.assertTrue(len(categorys) > 0)
